66f9823e57ea26fbe7a7e459727a0424ec4e6b41
Davidokih <davidyole023@gmail.com>
2024-07-21 14:03:51 +0100

    update

--- index.js
+++ index.js
@@ -2,45 +2,57 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const Parser = require('json2csv').Parser;
 
+
 const run_web_scrapper = async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.traversymedia.com/');
 
-    // await page.screenshot({ path: 'example.png', fullPage: true });
-    // const html = await page.content();
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://scholarship-positions.com/amr-advocacy-photo-contest/2023/11/03/');
+        // await page.waitForSelector('.region');
 
-    const scrappedData = await page.evaluate(() => {
-        const products = [];
-        const elements = document.querySelectorAll('.cscourse-grid .card');
-        elements.forEach(e => {
-            const level = e.querySelector('.card-body .level').innerText;
-            const title = e.querySelector('.card-body h3').innerText;
-            const url = e.querySelector('.card-footer a').href;
+        // await page.screenshot({ path: 'example.png', fullPage: true });
+        // const html = await page.content();
 
-            products.push({ level, title, url });
+        const scrapedData = [];
+        const title = await page.$eval('h1', (el => el.textContent));
+        console.log(title);
+        const about = await page.evaluate(() => document.querySelector('.entry-content').innerText);
+        console.log(about);
+        const applyUrl = await page.$eval('.entry-content h2 a', (el => el.href));
+        console.log(applyUrl);
+        // const filteredContent = content.replace(header, '').replace(footer, '');
+
+        if (title && about.length > 0 && applyUrl) {
+            scrapedData.push({ Title: title, About: about, ApplyURL: applyUrl });
+        }
+
+        // console.log(scrapedData);
+        // const csvParser = new Parser();
+        // const csv = csvParser.parse(scrapedData);
+
+        // fs.writeFile('scholarship.csv', csv, (err) => {
+        //     if (err) throw err;
+        //     console.log('File Saved');
+        // });
+        const existingData = await fs.promises.readFile('scholarship.csv', err => {
+            if (err) throw err;
+            console.log('Data read successfully');
         });
-        return products;
-    });
-    // const scrappedData = await page.$$eval('.cscourse-grid .card', (elements) => elements.map(e => ({
-    //     level: e.querySelector('.card-body .level').innerText,
-    //     title: e.querySelector('.card-body h3').innerText,
-    //     url: e.querySelector('.card-footer a').href
-    // })));
-    // console.log(scrappedData);
-    // const jsonData = JSON.stringify(scrappedData);
-    // console.log(jsonData);
 
-    const csvParser = new Parser();
-    const csv = csvParser.parse(scrappedData);
+        const updateData = existingData + csv;
 
-    fs.writeFile('products.csv', csv, (err) => {
-        if (err) throw err;
-        console.log('File Saved');
-    });
-    // console.log(csv);
-    // console.log('Data scrapped and saved to products.csv');
-    await browser.close();
+        await fs.promises.writeFile('scholarship.csv', updateData, err => {
+            if (err) throw err;
+            console.log('Data updated successfully');
+        });
+
+        // console.log(csv);
+        // console.log('Data scrapped and saved to products.csv');
+        await browser.close();
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 run_web_scrapper();
